6942756ebbe592fe427a32a97930f8f1b863968f
rwalkow <r.walkow@po.edu.pl>
2022-03-30 12:52:52 +0000

    Modify js code in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -132,7 +132,44 @@
     processOrder(){
       const thisProduct = this;
       console.log('processOrder:', thisProduct);
+
+      // convert form to objet structure e.g. { sauce: ['tomato'], toppings: ['olives', redPeppers]}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
       
+      // set price to default price 
+      let price = thisProduct.data.price;
+      
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { lablel: 'Toppings', type: 'checkoboxes'...}
+        const param = thisProduct.data.params[paramId];
+        console.log('param:', paramId, param);
+      
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log('option:', option.price);
+          // check if there is param with name of paramId in formData and if it includes optionId
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+            // check if the option is not default
+            if (option.default != true) {
+              console.log('default', option.default);
+              // add option price to variable
+              price = price + option.price;
+            }
+          } else {
+            // check if the option is default
+            if (option.default == true) {
+              // reduce price variable
+              price = price - option.price;
+            }
+          }
+        }
+      }
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
     }
   }
 
