9de3374737eb8061979cfdfa89281fb9e0a6a57a
rwalkow <r.walkow@po.edu.pl>
2022-03-30 11:52:29 +0000

    Modify js code in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -61,7 +61,9 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
     }
+    
     renderInMenu() {
       const thisProduct = this;
       /* generate HTML based on template */
@@ -73,8 +75,33 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
-  }
 
+    initAccordion() {
+      const thisProduct = this;
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelectorAll(
+        select.menuProduct.clickable
+      );
+      console.log('clickable', clickableTrigger);
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.forEach(clickableElement => {
+
+        clickableElement.addEventListener('click', function(event) {     
+        /* prevent default action for event */
+          event.preventDefault();
+          /* find active product (product that has active class) */
+          const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+          
+          /* if there is active product and it's not thisProduct.element, remove class active from it */
+          if (activeProduct != null && activeProduct != thisProduct.element) {
+            activeProduct.classList.remove('active');        
+          }
+          /* toggle active class on thisProduct.element */
+          thisProduct.element.classList.toggle('active');
+        });
+      });
+    }
+  }
 
   const app = {
     initMenu: function () {
